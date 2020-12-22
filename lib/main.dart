ec92088d052b0c4c87dca4c1dfe5cdc8d9ea640e
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2020-12-22 21:42:22 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("Orientation"),
+          ),
+          body: MainPage()),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return (MediaQuery.of(context).orientation == Orientation.portrait)
+        ? Column(children: generateContainer())
+        : Row(children: generateContainer());
+  }
+
+  List<Widget> generateContainer() {
+    return [
+      Container(
+        color: Colors.red,
+        width: 100,
+        height: 100,
+      ),
+      Container(
+        color: Colors.yellow,
+        width: 100,
+        height: 100,
+      ),
+      Container(
+        color: Colors.purple,
+        width: 100,
+        height: 100,
+      ),
+    ];
+  }
+}
